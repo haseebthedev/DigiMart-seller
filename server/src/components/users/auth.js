623f86ab40c2dbe85a6a0bbf94ac3b11b8fa0afb
unknown <haseebhowto@gmail.com>
2022-01-21 01:55:02 +0500

    Removed Server Folder | Updated Version

--- server/src/components/users/auth.js
+++
@@ -1,9 +0,0 @@
-const seller = require('./seller/middlewares/sellerAuth')
-const buyer = require('./buyer/middlewares/buyerAuth')
-const superAdmin = require('./super-admin/middlewares/superAdminAuth')
-
-module.exports = {
-    seller,
-    buyer,
-    superAdmin
-}
